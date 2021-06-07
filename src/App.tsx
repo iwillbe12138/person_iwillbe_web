0308d38f027230c76791e5b764d36fe1b60c9a14
iwillbe12138 <iwillbe12138@163.com>
2021-06-07 17:47:49 +0800

    import  antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+/*
+ * @Description: 
+ * @Author: iwillbe12138
+ * @Date: 2021-02-24 09:53:07
+ * @LastEditTime: 2021-06-07 17:47:07
+ * @LastEditors: iwillbe12138
+ */
 import React from 'react';
-import logo from './logo.svg';
+import {Button }from 'antd'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Button type="primary">啦啦啦</Button>
     </div>
   );
 }
