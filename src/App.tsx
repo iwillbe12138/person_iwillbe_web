e29e76de68850311d768c561070890bfbdc3d10b
iwillbe12138 <iwillbe12138@163.com>
2021-06-09 00:40:39 +0800

    vite react17

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 /*
  * @Description: 
  * @Author: iwillbe12138
- * @Date: 2021-02-24 09:53:07
- * @LastEditTime: 2021-06-07 17:47:07
+ * @Date: 2021-06-08 23:32:57
+ * @LastEditTime: 2021-06-09 00:34:30
  * @LastEditors: iwillbe12138
  */
-import React from 'react';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import {Button }from 'antd'
-import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-        <Button type="primary">啦啦啦</Button>
+    <div >
+       <Button type="primary">啦啦啦</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Description: 
+ * @Author: iwillbe12138
+ * @Date: 2021-06-08 23:32:57
+ * @LastEditTime: 2021-06-09 00:33:19
+ * @LastEditors: iwillbe12138
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import 'antd/dist/antd.css';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
