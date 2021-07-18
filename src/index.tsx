51e627143e5bfe967ccbc8b6f43ada38f4fd44fb
iwillbe12138 <iwillbe12138@163.com>
2021-07-18 18:52:48 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import { ConfigProvider } from 'antd';
+moment.locale('zh-cn');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN}>
+            <App />
+        </ConfigProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
